9a748dc09fd047dd652093674b65aca49d855967
Dell <5095meshwapatel@email.com>
2024-08-24 18:49:02 +0530

    24/8

---
+++ src/ASCENDING.java
@@ -0,0 +1,12 @@
+//WAP TO SORT ARRAY IN ASCENDING(LOW TO HIGH) ORDER IN JAVA
+
+import java.util.Arrays;
+
+public class ASCENDING {
+    public static void main(String[] args) {
+        int[] array = {5, 2, 8, 7, 1, 9};
+        Arrays.sort(array);
+
+        System.out.println("Array in ascending order: " + Arrays.toString(array));
+    }
+}
---
+++ src/Largest.java
@@ -0,0 +1,29 @@
+//WAP TO FIND THE LARGEST ELEMENT FROM THE ARRAY IN JAVA
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Largest {
+    public static void main(String[] arg) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> numList = new ArrayList<>();
+
+        System.out.print("Enter your length:");
+        int length = input.nextInt();
+        for (int i = 0; i < length; i++) {
+            System.out.print("Enter your number " + i);
+            int num = input.nextInt();
+            numList.add(num);
+        }
+        int largest = 0;
+        for (int i = 0; i < length; i++) {
+            if (largest < numList.get(i)) {
+                largest = numList.get(i);
+            }
+        }
+
+        System.out.print("largest number: " + largest);
+    }
+}
